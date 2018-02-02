78448ca07467940f18bc9abf4e3c1d2f28131ede
СайынБелек Алексеевич Кошкароол <sakoshkarool@ttit.local>
2018-02-02 09:19:27 +0700

    Add

---
+++ StaticDataAndMembers/StaticDataAndMembers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Static Data *****\n");
+            SavingAccount s1 = new SavingAccount(50);
+            SavingAccount s2 = new SavingAccount(100);
+            Console.WriteLine("Interest Rate is: {0}", SavingAccount.GetInterestRate());
+            SavingAccount.SetInterestRate(0.08);
+            SavingAccount s3 = new SavingAccount(10000.75);
+            Console.WriteLine("Interest rate is: {0}", SavingAccount.GetInterestRate());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticDataAndMembers/StaticDataAndMembers/SavingAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    class SavingAccount
+    {
+        public double currBalance;
+
+        public static double currInterestRate;
+        public SavingAccount(double balance)
+        {
+           
+            currBalance = balance;
+        }
+        static SavingAccount()
+        {
+            Console.WriteLine("In static ctor!");
+            currInterestRate = 0.04;
+        }
+
+        public static void SetInterestRate(double newRate)
+        {
+            currInterestRate = newRate;
+        }
+        public static double GetInterestRate()
+        {
+            return currInterestRate;
+        }
+    }
+}
